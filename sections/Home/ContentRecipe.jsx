94412534d9af41f05deff889685498f43fd7c360
Mateo Pereira <pereiramateo19@gmail.com>
2025-04-09 23:14:56 -0500

    se implemento el titulo reactivo si el usuario a hecho busquedas

--- sections/Home/ContentRecipe.jsx
+++ sections/Home/ContentRecipe.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WideCard from "@/components/Cards/WideCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import { TitleH2 } from "@/components/AuxComponents/Title";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
 
 const ContentRecipe = ({ data }) => {
+    const [search] = useLocalStorage("searchHistoryFlavor", []);
+    const [title, setTitle] = useState("");
+
+    useEffect(() => { setTitle(search.length === 0 ? "¡Nuestros favoritos!" : "Tus favoritos no se olvidan ❤️") }, [search])
+
     return (
         <div className="lg:px-26 md:px-12 px-4 w-full mt-12">
-            <TitleH2 text="¡Explora tus gustos!" />
+            <TitleH2 classes="italic" text={title} />
             <Swiper
                 slidesPerView={1}
                 spaceBetween={0}
